dbed7e9d248a9b2c413b1eedb770b3022e472cc9
J17253 <j17253@gmail.com>
2021-04-18 22:45:21 +0300

    Initial

---
+++ TicTacToeGame/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace TicTacToeGame
+{
+    public struct Position
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    public class TicTacToeGame
+    {
+        /// <summary>
+        /// null - clear, true - X, false - 0
+        /// </summary>
+        private bool?[,] _desk = new bool?[3,3];
+        private bool? _turnX;
+        private bool _finished;
+        
+        public void Start()
+        {
+            ClearDesk();
+            _turnX = null;
+        }
+
+        public bool TurnX(int row, int column)
+        {
+
+        }
+
+        private void ClearDesk()
+        {
+            for (var i = 0; i < 3; ++i)
+                for (var j = 0; j < 3; ++j)
+                {
+                    _desk[i, j] = null;
+                }
+        }
+    }
+}
